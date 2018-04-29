02f700b04bd42c7cb197adaf288b9549d643d35d
Jonathon Vega <jfvega@live.com>
2018-04-29 13:00:32 -0400

    Add files via upload

---
+++ DealershipUtility.java
@@ -0,0 +1,18 @@
+// Final Project
+public class DealershipUtility {
+   public static void main(String[] args){
+      
+   }
+   
+   // Methods under here
+   
+   public int autoPricecalc() {
+      return 1;
+   }
+   
+   public void loanCalc(int autoPrice) {
+   
+   }
+   
+   
+ }
