e2ee7e7ba7fcf50ed0b3feda11c05c95d540b491
Jonathon Vega <jfvega@live.com>
2018-05-06 12:51:01 -0400

    Created loop with do while

--- DealershipUtility.java
+++ DealershipUtility.java
@@ -9,23 +9,29 @@ import java.util.Scanner;
 public class DealershipUtility {
    
    public static void main(String[] args){
-      
       Scanner processInput = new Scanner(System.in);
-      System.out.println("What would you like to do?");
-      System.out.println("1 - Order Summary\n2 - Salesmen Standing\n3 - Dealership Revenue");
-      int userProcessChoice = processInput.nextInt();
+      int userProcessChoice;
       
-      switch (userProcessChoice) {
-         case 1:
-            break;
-         case 2:
-            break;
-         case 3:
-            break;
-         default:
-            System.out.println("Not a valid choice!");
-      }
+      do {
+         System.out.println("What would you like to do?");
+         System.out.println("1 - Order Summary\n2 - Salesmen Standing\n3 - Dealership Revenue\n4 - End Program");
+         userProcessChoice = processInput.nextInt();
       
+         switch (userProcessChoice) {
+            case 1:
+               break;
+            case 2:
+               break;
+            case 3:
+               //calculateTotalSale();
+               break;
+            case 4:
+               System.out.println("Exiting system");
+               break;
+            default:
+               System.out.println("Not a valid choice!");
+         }
+      }while(userProcessChoice != 4);
       
    }
    
@@ -36,6 +42,10 @@ public class DealershipUtility {
    }
    
    public void loanCalc(int autoPrice) {
+   
+   }
+   
+   public void calculateTotalSale(){
    
    }
    
