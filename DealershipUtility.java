fd3eeeee9f11bc4937b10df0dc1edf9c40117f2c
phatle90vn <38839188+phatle90vn@users.noreply.github.com>
2018-05-06 15:28:49 -0400

    Update DealershipUtility.java

--- DealershipUtility.java
+++ DealershipUtility.java
@@ -19,10 +19,8 @@ public class DealershipUtility {
       
          switch (userProcessChoice) {
             case 1:
-               autoPriceCalc();
                break;
             case 2:
-               salesmanStanding();
                break;
             case 3:
                calculateTotalSale();
@@ -37,97 +35,41 @@ public class DealershipUtility {
       
    }
    
-   // Process one
-   public static int autoPriceCalc(){
+   // Methods under here
    
-      String[] makes={"Hyundai", "Chevrolet", "Ford", "Toyota", "Volkswagon"};
-      int[] makeIndex={10000, 14000, 15000,16000,20000};
+   public int autoPricecalc() {
+      return 1;
+   }
+   
+   public void loanCalc(int autoPrice) {
+      Scanner input = new Scanner(System.in);
+  
+      System.out.print("Enter price quotes (ex: $5000) :$");
+      double priceQuotes = input.nextDouble();
+      double taxRate = .06;
+      double afterTaxPrice = priceQuotes + ( priceQuotes * .06 ); 
+      System.out.print("Enter the down payment :$");
+      double downPayment = input.nextDouble();
+      double loanAmount = afterTaxPrice - downPayment;
+      System.out.print("Enter APR %");
+      double aPR = input.nextDouble();
+      double periodRate = (aPR / 100) / 12;
+      System.out.print("Enter loan term in year ");
+      int loanTerm = input.nextInt();
+      int totalNumberOfPayments = loanTerm * 12 ;
+
       
-      String[] models={"Compact Sedan", "Full-size Sedan", "Sportscar", "Midsize SUV", "Full-size SUV", "Truck"};
-      int[] modelIndex={0, 4500, 8750, 6250, 8500, 25000};
-      
-      String[] trim ={"Standard", "Special Edition", "Sport", "Luxury"};
-      int[] trimIndex={0, 1000, 2200, 3500};
-      
-      String[] colors= {"Black", "Silver", "Green", "White", "Blue", "Red"};
-      int[] paintIndex={0,0,0,0,250,500};
-      
-      Scanner input=new Scanner(System.in);
-      
-      int makeChoice;
-      int modelChoice;
-      int trimChoice;
-      int colorChoice;
-      
-      //Make Selection Process
-      do{
-         System.out.println("Enter the number associated with your choice in vehicle make");
-         System.out.println("0) Hyundai |1) Chevrolet |2) Ford |3) Toyota |4) Volkswagon");
-         try{
-            makeChoice=input.nextInt();
-         }catch(InputMismatchException e){
-            makeChoice=-1;
-            input.next();
-            }
-         if(makeChoice>=makeIndex.length||makeChoice<0){
-            System.out.println("Incorrect input please try again");
-            }
-      }while(makeChoice>=makeIndex.length||makeChoice<0);
-      
-      //Model Selection Process
-      do{
-         System.out.println("Enter the number associated with your choice in model type");
-         System.out.println("0) Compact Sedan |1) Full-size Sedan |2) Sportscar |3) Midsize Suv |4) Full-size SUV |5) Truck");
-         try{
-         modelChoice=input.nextInt();
-         }catch(InputMismatchException e){
-         modelChoice=-1;
-         input.next();
-         }
-         if(modelChoice>=modelIndex.length){
-            System.out.println("Incorrect input, please try again");
-            }
-      }while(modelChoice>=modelIndex.length||modelChoice<0);
-      
-      //Trim Selection Process
-      do{
-         System.out.println("Enter the number associated with your choice in trim type");
-         System.out.println("0) Standard |1) Special Edition |2) Sport |3) Luxury");
-         try{
-         trimChoice=input.nextInt();
-         }catch(InputMismatchException e){
-         trimChoice=-1;
-         input.next();
-         }
-         if(trimChoice>=trimIndex.length){
-            System.out.println("Incorrect input please try again");
-            }
-      }while(trimChoice>=trimIndex.length||trimChoice<0);
-      
-      //Color Selection Process
-      do{
-         System.out.println("Enter the number associated with your choice in vehicle color");
-         System.out.println("0) Black |1) Silver |2) Green |3) White |4) Blue |5) Red");
-         try{
-         colorChoice=input.nextInt();
-         }catch(InputMismatchException e){
-         colorChoice=-1;
-         input.next();}
-         if(colorChoice>=paintIndex.length){
-            System.out.println("Incorrect input please try again");
-            }
-      }while(colorChoice>=paintIndex.length||colorChoice<0);
-      
-      //Print out Customer Selection and estimated price before tax
-      int finalEstimate = (makeIndex[makeChoice]+modelIndex[modelChoice]+trimIndex[trimChoice]+paintIndex[colorChoice]);
-      System.out.println("Car Summary and Cost Estimation:\n");
-      System.out.printf("Make: %15s\n", makes[makeChoice]);
-      System.out.printf("Model: %14s\n", models[modelChoice]);
-      System.out.printf("Trim: %15s\n", trim[trimChoice]);
-      System.out.printf("Color: %14s\n", colors[colorChoice]);
-      System.out.printf("\n(Pre-tax)Price:$%14d\n",finalEstimate);
-      
-      return finalEstimate;
+      double periodPayment = loanAmount / discountFactor(periodRate,totalNumberOfPayments);
+      System.out.println("Total amount due now is $" + downPayment);
+      System.out.println("Monthly payment is $" + periodPayment);
+      System.out.println("Loan amount is $" + loanAmount);
+      System.out.println("Tax rate is %" + taxRate);
+
+   
+   }
+   public static double discountFactor(double periodRate,int totalNumberOfPayments) {
+      double discountFactor = (( Math.pow((1 + periodRate),totalNumberOfPayments) ) - 1) / (periodRate * Math.pow((1 + periodRate),totalNumberOfPayments));
+      return discountFactor;
    }
    
    // Process Two: Salesmen Standing
