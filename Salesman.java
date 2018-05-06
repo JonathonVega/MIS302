de481fa7fb0cdfc7d01d7e0db62a32581d2a9770
haileyquigley <38839190+haileyquigley@users.noreply.github.com>
2018-05-06 14:57:54 -0400

    Salesman Class Added

---
+++ Salesman.java
@@ -0,0 +1,14 @@
+public class Salesman{
+   double sales;
+   String name;
+   
+   public Salesman(double sales, String name) {
+   this.sales = sales;
+   this.name = name;
+   
+   
+   }
+      
+
+
+}
