8ba937e46c0a0578d870ce9e188e3a715c5b25d1
Jonathon Vega <jfvega@live.com>
2018-05-08 23:46:00 -0400

    Added comment section on top

--- Salesman.java
+++ Salesman.java
@@ -1,3 +1,10 @@
+/*5/9/2018
+**Jonathon Vega, Phat Le, Alexander Battle, Hailey Quigley
+**MIS302 Final Project
+**This is a class for a Salesman object that stores the name of a salesman and the total sales
+**they've made.
+*/
+
 public class Salesman{
    double sales;
    String name;
@@ -5,10 +12,5 @@ public class Salesman{
    public Salesman(double sales, String name) {
    this.sales = sales;
    this.name = name;
-   
-   
    }
-      
-
-
 }
